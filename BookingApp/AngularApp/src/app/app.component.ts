5af787044fcc00af31f424f5e570efefd9c041ad
ognjen94 <ognjen994@gmail.com>
2017-06-14 12:57:48 +0200

    login window

--- BookingApp/AngularApp/src/app/app.component.ts
+++ BookingApp/AngularApp/src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
+import { AppUser } from './model/app-user.model';
+
+import {
+  Router,
+  ActivatedRoute
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private authService: AuthService) {
+    
+   }
+
+   logIn(username:string, password:string){
+    this.authService.logIn();
+  }
+  
+    logOut(){
+        this.authService.logOut();
+    }
+
+    isLoggedIn() : boolean{
+        return this.authService.isLoggedIn();
+    }
 }
--- BookingApp/AngularApp/src/app/app.module.ts
+++ BookingApp/AngularApp/src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './services/auth.service';
+import { LoggedInGuard } from './logged-in.guard'
+
+//const Routes 
+  //{path: }
+//]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService, LoggedInGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
